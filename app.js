4e7d6a843918ebbc418b25aa444f499edbdfebda
janteuni <janteuni@student.42.fr>
2015-05-22 17:37:04 +0200

    feat(car): add moving car

--- app.js
+++ app.js
@@ -1,34 +1,49 @@
-var IMGURL = "images/back-mountain.svg";
-var IMGW = 500;
+
 var container = document.querySelector(".container");
-var imgs = [];
+
+var allImgs = [
+    {url: "images/road.svg", class:"s-300", size: 300, speed: 1, y: 250,  imgs: []},
+    {url: "images/back-mountain.svg", class:"s-900", size: 900, speed: 20, y: 0, imgs: []},
+    {url: "images/landscape.svg", class:"s-900", size: 900, speed: 10, y: 10, imgs: []}
+
+];
 
 function preLoadImg(callback) {
-    var img = new Image();
-    img.onload = callback;
-    img.src = IMGURL;
-}
-
-preLoadImg(function() {
-    for (var i = 0; i < window.innerWidth; i += (IMGW)) {
-        addImg();
-    }
-    addImg();
-    animate();
-});
-
-function animate () {
-    imgs.forEach(function (img, i) {
-        var t = new TimelineMax({ repeat: -1 });
-        t
-            .to(img, 3, { x: '-=' + (IMGW), ease: Linear.easeNone });
+    allImgs.forEach(function(data) {
+        var img = new Image();
+        img.onload = function () { callback(data); };
+        img.src = data.url;
     });
 }
 
-function addImg() {
-    var img = document.createElement("img");
-    img.src = IMGURL;
-    TweenMax.set(img, {x:(IMGW) * imgs.length});
-    imgs.push(img);
-    container.appendChild(img);
-};
+preLoadImg(function(image) {
+    for (var i = 0; i < window.innerWidth; i += image.size) {
+        addImg(image);
+    }
+    addImg(image);
+    animate(image);
+});
+
+function animate (data) {
+    data.imgs.forEach(function (img) {
+        var t = new TimelineMax({ repeat: -1 });
+        t.to(img.element, data.speed, { x: '-=' + (img.size), ease: Linear.easeNone });
+    });
+}
+
+function addImg(data) {
+    var element = document.createElement("img");
+    element.src = data.url;
+    element.className = data.class;
+    TweenMax.set(element, {x:(data.size - 2) * data.imgs.length, y: data.y});
+    data.imgs.push({element: element, size: data.size});
+    container.appendChild(element);
+}
+
+(function(){
+    var car = document.querySelector(".camion");
+    var t = new TimelineMax({ repeat: -1 });
+    t
+        .to(car, 0.3, { y: -6})
+        .to(car, 0.3, { y: 0});
+})();
